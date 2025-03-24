ab1cc87909c2982bdd2509657b6782e812484e6e
Rodina Mo'men <Rodinamobark3@gmail.com>
2025-03-24 12:47:20 +0200

    Adding di for remotedatasource Adding di for home feature

---
+++ app/src/main/java/com/example/aroundegypt/features/home/di/HomeDi.kt
@@ -0,0 +1,45 @@
+package com.example.aroundegypt.features.home.di
+
+import com.example.aroundegypt.common.domain.remote.IRestApiProvider
+import com.example.aroundegypt.features.home.data.repo.HomeRepo
+import com.example.aroundegypt.features.home.data.repo.remote.HomeRemoteDataSourceRepo
+import com.example.aroundegypt.features.home.domain.interactor.GetExperiencesUC
+import com.example.aroundegypt.features.home.domain.interactor.GetRecommendedExperiencesUC
+import com.example.aroundegypt.features.home.domain.interactor.LikeExperienceUC
+import com.example.aroundegypt.features.home.domain.interactor.SearchUC
+import com.example.aroundegypt.features.home.domain.repo.IHomeRepo
+import com.example.aroundegypt.features.home.domain.repo.remote.IHomeRemoteDataSourceRepo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+internal object HomeDi {
+    @Provides
+    fun provideHomeRemoteDatasource(restApiNetworkProvider: IRestApiProvider): IHomeRemoteDataSourceRepo =
+        HomeRemoteDataSourceRepo(restApiNetworkProvider)
+
+
+    @Provides
+    fun provideHomeRepo(
+        remoteDS: IHomeRemoteDataSourceRepo,
+
+    ): IHomeRepo =
+        HomeRepo(remoteDS)
+
+    @Provides
+    fun provideGetExperiencesUC(repository: IHomeRepo): GetExperiencesUC =
+        GetExperiencesUC(repository)
+
+    @Provides
+    fun provideGetRecommendedExperiencesUC(repository: IHomeRepo): GetRecommendedExperiencesUC =
+        GetRecommendedExperiencesUC(repository)
+    @Provides
+    fun provideLikeExperiencesUC(repository: IHomeRepo): LikeExperienceUC =
+        LikeExperienceUC(repository)
+    @Provides
+    fun provideSearchExperiencesUC(repository: IHomeRepo): SearchUC =
+        SearchUC(repository)
+}
