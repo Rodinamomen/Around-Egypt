424ff9a63f1b1c903452eb89fab7aa4909d4ed0c
Rodina Mo'men <Rodinamobark3@gmail.com>
2025-03-24 12:46:00 +0200

    Adding usecases needed for search ,like ,get recommended , and recent experiences

---
+++ app/src/main/java/com/example/aroundegypt/features/home/domain/interactor/GetRecommendedExperiencesUC.kt
@@ -0,0 +1,27 @@
+package com.example.aroundegypt.features.home.domain.interactor
+
+import com.example.aroundegypt.common.data.models.Resource
+import com.example.aroundegypt.common.data.models.exception.AroundEgyptException
+import com.example.aroundegypt.features.home.data.repo.HomeRepo
+import com.example.aroundegypt.features.home.domain.models.Experiences
+import com.example.aroundegypt.features.home.domain.repo.IHomeRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+
+internal class GetRecommendedExperiencesUC(private val homeRepo: IHomeRepo) {
+    operator fun invoke(): Flow<Resource<Experiences>> = flow {
+        emit(Resource.Loading(true))
+        val remoteResponse = homeRepo.getRecommendedExperiences()
+        emit(Resource.Success(remoteResponse))
+    }.catch { throwable ->
+        val failureResource = if (throwable is AroundEgyptException) throwable else
+            AroundEgyptException.UnKnownException.UnKnown(message = throwable.message.toString())
+        emit(Resource.Failure(failureResource))
+    }.onCompletion {
+        emit(Resource.Loading(false))
+    }.flowOn(Dispatchers.IO)
+}
