f43ae54aad6ba2f04dff702e6acf72751462ac3e
Rodina Mo'men <Rodinamobark3@gmail.com>
2025-03-24 12:45:17 +0200

    Adding repo implementation

---
+++ app/src/main/java/com/example/aroundegypt/features/home/data/repo/HomeRepo.kt
@@ -0,0 +1,26 @@
+package com.example.aroundegypt.features.home.data.repo
+
+import com.example.aroundegypt.features.home.data.mappers.ExperiencesMapper
+import com.example.aroundegypt.features.home.data.mappers.LikesResponseMapper
+import com.example.aroundegypt.features.home.domain.models.Experiences
+import com.example.aroundegypt.features.home.domain.models.LikesResponse
+import com.example.aroundegypt.features.home.domain.repo.IHomeRepo
+import com.example.aroundegypt.features.home.domain.repo.remote.IHomeRemoteDataSourceRepo
+
+internal class HomeRepo(private val remoteDataSourceRepo: IHomeRemoteDataSourceRepo):IHomeRepo {
+    override suspend fun getExperiences(): Experiences {
+       return ExperiencesMapper.dtoToDomain(remoteDataSourceRepo.getExperiences())
+    }
+
+    override suspend fun getRecommendedExperiences(): Experiences {
+        return ExperiencesMapper.dtoToDomain(remoteDataSourceRepo.getRecommendedExperiences())
+    }
+
+    override suspend fun likeExperience(id: String): LikesResponse {
+        return LikesResponseMapper.dtoToDomain(remoteDataSourceRepo.likeExperience(id))
+    }
+
+    override suspend fun search(title: String): Experiences {
+        return ExperiencesMapper.dtoToDomain(remoteDataSourceRepo.search(title))
+    }
+}
